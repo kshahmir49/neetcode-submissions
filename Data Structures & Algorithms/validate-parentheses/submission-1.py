fc40d6a119406acd183c38916dc18131d868ff53
Shahmir Khan Mohammed <kshahmir49@gmail.com>
2026-04-28 12:54:47 +0400

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.py
@@ -0,0 +1,10 @@
+import collections
+class Solution:
+    def isValid(self, s: str) -> bool:
+        d = collections.deque()
+        for i in s:
+            if d and ((d[-1]=="(" and i==")") or (d[-1]=="{" and i=="}") or (d[-1]=="[" and i=="]")):
+                d.pop()
+            else:
+                d.append(i)
+        return not d
