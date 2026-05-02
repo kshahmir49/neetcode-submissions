39db3e481a32c07880c522fc46f41de85ed1e1ec
Shahmir Khan Mohammed <kshahmir49@gmail.com>
2026-05-02 13:18:28 +0400

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.py
@@ -0,0 +1,20 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        cloned = {}
+        def clone(node):
+            if node in cloned:
+                return cloned[node]
+            copy = Node(node.val)
+            cloned[node] = copy
+            for n in node.neighbors:
+                copy.neighbors.append(clone(n))
+            return copy
+        return clone(node) if node else None
